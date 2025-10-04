cc70a5a33018f7aabc2de8cf1376e719d6e45d0a
euemm <euem@euem.net>
2025-10-04 01:16:33 -0400

    node script update
    pm2 support

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: 'ssh-client',
+      script: 'npx',
+      args: 'serve@latest out',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '0.2G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      },
+      log_file: './logs/combined.log',
+      out_file: './logs/out.log',
+      error_file: './logs/error.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
+    }
+  ]
+};
