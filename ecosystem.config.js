fcda0f1918dd0d4cfd9b753e770ffd95e3951745
euem <euem@euem.net>
2025-10-04 05:18:27 +0000

    251004 pm2 support complete

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '0.2G',
+      max_memory_restart: '250M',
       env: {
         NODE_ENV: 'production',
         PORT: 3000
